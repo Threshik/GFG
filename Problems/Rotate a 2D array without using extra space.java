ce5ae584481ff1869fde15ad38cb2a60cb888b62
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-07-05 18:13:45 +0530

    Create Rotate a 2D array without using extra space.java

---
+++ Problems/Rotate a 2D array without using extra space.java
@@ -0,0 +1,16 @@
+class Solution {
+    void rotateMatrix(int arr[][], int n) {
+        // code here
+        for(int i=0;i<n/2;i++)
+        {
+            for(int j=i;j<n-i-1;j++)
+            {
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][n-1-i];
+                arr[j][n-1-i]=arr[n-1-i][n-1-j];
+                arr[n-1-i][n-1-j]=arr[n-1-j][i];
+                arr[n-1-j][i]=temp;
+            }
+        }
+    }
+}
