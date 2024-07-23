f9991686b598f5a88b65b7afd21fbac99ef1e7ac
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-07-23 18:08:20 +0530

    Create Floor in BST.java

---
+++ Tree/Floor in BST.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int floor(Node root, int x) {
+        // Code here
+        if(root==null)
+        return -1;
+        int floor=-1;
+        while(root!=null)
+        {
+            if(root.data==x)
+            return x;
+            if(root.data<x)
+            {
+                floor=root.data;
+                root=root.right;
+            }
+            else
+            root=root.left;
+        }
+        return floor;
+    }
